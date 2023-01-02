52c5d8730fa4dd6c057efc6f00ba4f5900286d34
danielvec <vechioladaniel@gmail.com>
2023-01-02 12:53:08 -0800

    add show method for events

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   # root "articles#index"
   root "events#index"
 
-  resources :events, only: [:new, :create, :index]
+  resources :events, only: [:new, :create, :index, :show]
 end
