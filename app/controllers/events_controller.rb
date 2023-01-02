f1dfbd346f6d9426603099016b5d9910729dc633
danielvec <vechioladaniel@gmail.com>
2023-01-01 23:45:18 -0800

    fix typo in create action

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -10,7 +10,7 @@ class EventsController < ApplicationController
   end
 
   def create
-    @event = current_user.created_events.build(post_params)
+    @event = current_user.created_events.build(event_params)
 
     if @event.save
       redirect_to root_path
