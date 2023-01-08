5b7d7edd872be121f81060154cb1a4dcaaaa13ad
danielvec <vechioladaniel@gmail.com>
2023-01-08 14:13:26 -0800

    create class methods on Event model for past and upcoming events

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,12 @@ class Event < ApplicationRecord
   has_many :event_attendings, foreign_key: :attended_event_id
   has_many :attendees, through: :event_attendings, source: :event_attendee
   belongs_to :creator, class_name: "User"
+
+  def self.past
+    Event.where('events.date < ?', DateTime.now.in_time_zone('UTC'))
+  end
+
+  def self.upcoming
+    Event.where('events.date > ?', DateTime.now.in_time_zone('UTC'))
+  end
 end
