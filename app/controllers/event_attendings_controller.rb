95b1d9dbb7a657becd023cce5ecf5106a64228e1
danielvec <vechioladaniel@gmail.com>
2023-01-08 12:03:52 -0800

    add destroy action to event attendings

--- app/controllers/event_attendings_controller.rb
+++ app/controllers/event_attendings_controller.rb
@@ -4,4 +4,11 @@ class EventAttendingsController < ApplicationController
     @event_attending = current_user.event_attendings.create(attended_event_id: @event.id)
     redirect_to event_path(@event)
   end
+
+  def destroy
+    @event = Event.find(params[:event_id])
+    @event_attending = current_user.event_attendings.find_by(event_attendee_id: current_user.id)
+    @event_attending.destroy
+    redirect_to event_path(@event)
+  end
 end
