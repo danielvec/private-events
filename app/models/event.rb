e78ae13f0ba6438fb51d8520283729e0f03ebaf6
danielvec <vechioladaniel@gmail.com>
2023-01-19 18:01:38 -0800

    add image file to event model, set image and attending to dependent destroy

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,7 @@
 class Event < ApplicationRecord
-  has_many :event_attendings, foreign_key: :attended_event_id
+  has_many :event_attendings, foreign_key: :attended_event_id, dependent: :destroy
   has_many :attendees, through: :event_attendings, source: :event_attendee
+  has_one_attached :image, dependent: :delete_all
   belongs_to :creator, class_name: "User"
 
   scope :past, -> { where('events.date < ?', DateTime.now.in_time_zone('UTC')) }
