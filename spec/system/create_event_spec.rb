75603df7005869889d82668aada8943050e37094
danielvec <vechioladaniel@gmail.com>
2023-01-23 21:35:56 -0800

    remove unneccesary redirection, add more expected content, test for invalid input

--- spec/system/create_event_spec.rb
+++ spec/system/create_event_spec.rb
@@ -9,8 +9,17 @@ RSpec.describe 'Creating an event', type: :system do
     fill_in 'event[location]', with: 'Los Angeles'
     click_on 'Submit'
 
-    visit events_path
-    visit events_path
     expect(page).to have_content('Rails Conference')
+    expect(page).to have_content('Wed, Aug 16, 6:00 PM')
+    expect(page).to have_content('Los Angeles')
+  end
+
+  scenario 'invalid inputs' do
+    login_as(create(:user))
+    visit new_event_path
+    click_on 'Submit'
+
+    message = page.find("#event_title").native.attribute("validationMessage")
+    expect(message).to eq('Please fill out this field.')
   end
 end
